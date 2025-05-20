ead7b6cf43ec25d0cf6401d3e8b428d954760207
Cbrunig <cbbrunig@gmail.com>
2025-05-20 16:33:20 -0500

    changed planet placeholder list to table

--- planet-watch/src/components/InfoPage.jsx
+++ planet-watch/src/components/InfoPage.jsx
@@ -5,20 +5,38 @@ function InfoPage () {
         <div>
             <header>
                 <h1>Planet Watch</h1>
-                <br />
+            </header>
                 <div>
                     <img src={placeholder} alt="Map placeholder" height='500px' width='500px'/>
                 </div>
-                <ul>
-                    <li>Planet 1</li>
-                    <li>Planet 2</li>
-                    <li>Planet 3</li>
-                </ul>
-            </header>
+                <table>
+                    <tr>
+                        <th>Planet</th>
+                        <th>Magnitude</th>
+                        <th>RA</th>
+                        <th>Declination</th>
+                        <th>Altitude</th>
+                        <th>Direction</th>
+                    </tr>
+                    <tr>
+                        <th>Jupiter</th>
+                        <th>-1.9</th>
+                        <th>05h 40m 37s</th>
+                        <th>+23° 09' 44"</th>
+                        <th>63.0°</th>
+                        <th>South-West</th>
+                    </tr>
+                    <tr>
+                        <th>Mars</th>
+                        <th>1.1</th>
+                        <th>09h 12m 55s</th>
+                        <th>+17° 48' 58"</th>
+                        <th>58.7°</th>
+                        <th>South-East</th>
+                    </tr>
+                </table>
         </div>
     );
 }
-
-// TODO: change ul to table for ease of displaying more data
 
 export default InfoPage;
