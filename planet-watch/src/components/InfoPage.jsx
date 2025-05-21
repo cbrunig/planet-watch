54971939515a66b74b5ea794f9696c67b9c26050
Cbrunig <cbbrunig@gmail.com>
2025-05-21 09:03:47 -0500

    moved table to separate component

--- planet-watch/src/components/InfoPage.jsx
+++ planet-watch/src/components/InfoPage.jsx
@@ -1,4 +1,5 @@
 import placeholder from '../assets/svg-placeholder.png'
+import PlanetTable from './PlanetTable';
 
 function InfoPage () {
     return (
@@ -9,32 +10,7 @@ function InfoPage () {
                 <div>
                     <img src={placeholder} alt="Map placeholder" height='500px' width='500px'/>
                 </div>
-                <table>
-                    <tr>
-                        <th>Planet</th>
-                        <th>Magnitude</th>
-                        <th>RA</th>
-                        <th>Declination</th>
-                        <th>Altitude</th>
-                        <th>Direction</th>
-                    </tr>
-                    <tr>
-                        <th>Jupiter</th>
-                        <th>-1.9</th>
-                        <th>05h 40m 37s</th>
-                        <th>+23° 09' 44"</th>
-                        <th>63.0°</th>
-                        <th>South-West</th>
-                    </tr>
-                    <tr>
-                        <th>Mars</th>
-                        <th>1.1</th>
-                        <th>09h 12m 55s</th>
-                        <th>+17° 48' 58"</th>
-                        <th>58.7°</th>
-                        <th>South-East</th>
-                    </tr>
-                </table>
+                <PlanetTable />
         </div>
     );
 }
---
+++ planet-watch/src/components/PlanetTable.jsx
@@ -0,0 +1,32 @@
+function PlanetTable  () {
+    return (
+        <table>
+                    <tr>
+                        <th>Planet</th>
+                        <th>Magnitude</th>
+                        <th>RA</th>
+                        <th>Declination</th>
+                        <th>Altitude</th>
+                        <th>Direction</th>
+                    </tr>
+                    <tr>
+                        <th>Jupiter</th>
+                        <th>-1.9</th>
+                        <th>05h 40m 37s</th>
+                        <th>+23° 09' 44"</th>
+                        <th>63.0°</th>
+                        <th>South-West</th>
+                    </tr>
+                    <tr>
+                        <th>Mars</th>
+                        <th>1.1</th>
+                        <th>09h 12m 55s</th>
+                        <th>+17° 48' 58"</th>
+                        <th>58.7°</th>
+                        <th>South-East</th>
+                    </tr>
+                </table>
+    );
+}
+
+export default PlanetTable;
